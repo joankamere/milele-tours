d25e8c3f21833d0172e9e5737e04a24154346d52
joankamere <joankamere@gmail.com>
2021-03-12 09:12:05 +0300

    modify booking form

--- js/scripts.js
+++ js/scripts.js
@@ -21,21 +21,11 @@ const display = new Display();
 const store = new Store();
 
 //add a bboking info on submit
-document.getElementById('travel-booking-form').addEventListener('submit', function(event){
 document.getElementById('travel-booking-form').addEventListener('submit', function (event) {
 
   //prevent submission without inputs
   event.preventDefault();
-    const location = document.querySelector('#location');
-    const checkIn  = document.querySelector('#checkin');
-    const checkOut = document.querySelector('#checkout');
-    const noPeople = document.querySelector('#nopeople');
 
-  //create an object for the constructor function
-  const booking = new Booking(this.location.value, this.checkin.value, this.checkout.value,this.nopeople.value );
-
-  //validate input fields
-  display.validateField(booking,store);
   const location = document.querySelector('#location');
   const checkIn = document.querySelector('#checkin');
   const checkOut = document.querySelector('#checkout');
@@ -51,32 +41,21 @@ document.getElementById('travel-booking-form').addEventListener('submit', functi
 });
 
 //display constructor function
-function Display(){
 function Display() {
   this.location = document.querySelector('#location');
   this.checkIn = document.querySelector('#checkin');
   this.checkOut = document.querySelector('#checkout');
   this.noPeople = document.querySelector('#nopeople');
   this.bookings = document.getElementById("book");;
-  
-}
-
-//pizza constructor function
-function Booking(location,checkIn,checkOut,noPeople){
 
 }
 
 //pizza constructor function
 function Booking(location, checkIn, checkOut, noPeople) {
-
   this.location = location
   this.checkIn = checkIn;
   this.checkOut = checkOut;
   this.noPeople = noPeople;
-  
-}
-//Local storage constructor
-function Store(){
 
 }
 //Local storage constructor
@@ -86,57 +65,6 @@ function Store() {
 
 
 //checking validation for fields
-Display.prototype.validateField = function(booking,store){
-  //check if inputs are empty
-
- 
-
-  if(this.location.value === '' ||this.checkIn.value === ''|| this.checkOut.value  === ''|| this.noPeople.value  === ''){
-      alert("Please fill all fields");
-
-  }else{
-      alert("You have successfully made your Booking Order");
-      this.clearFields();
-      this.addBooking(booking);
-      store.addBookings(booking);
-  }
-}
-
-Display.prototype.addBooking = function(booking){
-
-  let bookingPrice=0;
-  if(booking.location == 'Nairobi National Park' ){
-      bookingPrice = 2500;
-  }else if(booking.location == 'Malindi'){
-      bookingPrice = 20000;
-  }else if(booking.location == 'Diani'){
-      bookingPrice = 20000;
-  }else if(booking.location == 'Sagana'){
-    bookingPrice = 5500;
-  }else if(booking.location == 'Mt.Longonot' ){
-    bookingPrice = 2000;
-   }else if(booking.location == 'Mt.Aberdares'){
-    bookingPrice = 7000;
-   }else if(booking.location == 'Lamu'){
-    bookingPrice = 50,000;
-   }else if(booking.location == 'Chalbi'){
-    bookingPrice = 100000;
-   }else if(booking.location == 'Nanyuki'){
-    bookingPrice = 17000;
-  }else if(booking.location == 'Mombasa'){
-      bookingPrice = 12000;
-  }else if(booking.location == 'Amboseli'){
-    bookingPrice = 36000;
-  }else if(booking.location == 'Kisumu' ){
-    bookingPrice = 15000;
-  }else if(booking.location == 'Mt.Kenya'){
-    bookingPrice = 12925;
-  }else if(booking.location == 'Naivasha'){
-    bookingPrice = 8000;
-  }else if(booking.location == 'Maasai Mara'){
-    bookingPrice = 10000;
-  }
-   else{
 Display.prototype.validateField = function (booking, store) {
   //check if inputs are empty
 
@@ -195,16 +123,11 @@ Display.prototype.addBooking = function (booking) {
   let totalPrice = booking.noPeople * bookingPrice;
 
 
-const tableRow = document.createElement('tr')
- 
-  //appending new booking info to the booking list.
-  tableRow.innerHTML= `
 
   const tableRow = document.createElement('tr')
 
   //appending new booking info to the booking list.
   tableRow.innerHTML = `
-
   <tr>
   <td id="bookingLocation">${booking.location}</td>
   <td id="bookingCheckIn">${booking.checkIn}</td>
@@ -219,25 +142,7 @@ const tableRow = document.createElement('tr')
 
 }
 
-Display.prototype.showTotals = function(){
-  //an array to get the totals for the item count and item price
-  const totals = [];
-  const itemPrice = document.querySelectorAll('#bookingPrice');
-  
-  itemPrice.forEach((item)=>{
-      //pushing items to my array using the .push Javascript array method
-      totals.push(item.textContent)
-      console.log(item.textContent)
-  })
-  console.log(itemPrice)
-  console.log(totals)
-  
-  let intTotals = totals.map((i)=>Number(i));
-  console.log(intTotals)
-  
-  let totalMoney = intTotals.reduce(function(a,b){
-      return a+b;
-  },0)
+
 Display.prototype.showTotals = function () {
   //an array to get the totals for the item count and item price
   const totals = [];
@@ -262,7 +167,6 @@ Display.prototype.showTotals = function () {
 
 }
 
-Display.prototype.clearFields = function(){
 Display.prototype.clearFields = function () {
   this.location.value = '';
   this.checkIn.value = '';
@@ -270,33 +174,6 @@ Display.prototype.clearFields = function () {
   this.noPeople.value = '';
 }
 
-Store.prototype.getBookings= function(){
-  let bookings;
-  if(localStorage.getItem('bookings') === null){
-      bookings = []
-  }else{
-      bookings = JSON.parse(localStorage.getItem('bookings'))
-      console.log("Got Bookings")
-  }
-  console.log("getBoookings successfully runned ")
-  return bookings;
-  
-
-}
-
-Store.prototype.addBookings = function(booking){
-  let bookings = this.getBookings();
-  bookings.push(booking);
-
-  localStorage.setItem('bookings' ,JSON.stringify(bookings))
-  console.log("addBookings successfully runned")
-}
-
-Display.prototype.displayBookings = function(){
-  const bookings = store.getBookings();
-
-  bookings.forEach((booking)=>{
-      this.addBooking(booking);
 Store.prototype.getBookings = function () {
   let bookings;
   if (localStorage.getItem('bookings') === null) {
@@ -328,13 +205,6 @@ Display.prototype.displayBookings = function () {
   console.log("display bookings successfully runned")
 }
 
-document.addEventListener('DOMContentLoaded', 
-display.displayBookings())
-
-document.querySelector("#btnClear").addEventListener('click', function(){
-  let response = prompt("Are you sure you want to cancel your bookings? YES / NO ").toUpperCase() ;
-//  let response = res.toUppercase();
-  if(response === 'YES'){
 document.addEventListener('DOMContentLoaded',
   display.displayBookings())
 
@@ -345,23 +215,7 @@ document.querySelector("#btnClear").addEventListener('click', function () {
     window.localStorage.removeItem('bookings');
     window.location.reload();
   }
-  
 
-});
-
-$(document).ready(function(){
-       
-       
-  $('#bookings-total').hide()
-  $('#btnClear').hide()
- 
-  $('#btnCheckout').click(function(){
-
-    if(window.localStorage.getItem('bookings') === null){
-      $('#btnClear').hide();
-      alert("No Travel Packages Booked ! ")
-
-    }else if( (window.localStorage.getItem('bookings') !== null)){
 
 
 });
@@ -379,23 +233,15 @@ $(document).ready(function () {
       alert("No Travel Packages Booked ! ")
 
     } else if ((window.localStorage.getItem('bookings') !== null)) {
-
       $('#btnClear').show()
       $('#bookings-total').show()
 
-    }else{
 
     } else {
-
       $('#bookings-total').show()
 
 
     }
-    
-  });
-})
-
-
 
   });
 });
@@ -403,6 +249,12 @@ $(document).ready(function () {
 $(document).ready(function() {
   $("#btnCheckout").click(function() {
     $(".final-msg").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".final-msg").click(function() {
+    $(".payment-form").toggle();
+ 
   });
 });
 
